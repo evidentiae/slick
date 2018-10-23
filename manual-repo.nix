4580a8f5fe110ed6b3d64739a7b93f2e97e0ff9b
David Waern <david.waern@gmail.com>
2018-10-23 19:04:17 +0200

    Add a manual-repo.nix

---
+++ manual-repo.nix
@@ -0,0 +1,17 @@
+# this file must still be generated manually.  
+{
+  "repos" = {
+    "nix-public" = "";
+  };
+  "artifacts" = {
+    # dependencies so sbt can build the sbt-compiler-interface (http://www.scala-sbt.org/0.13/docs/Compiler-Interface.html)
+    "nix-public/org/scala-lang/jline/2.10.7/jline-2.10.7.pom" = {
+      url = "https://repo1.maven.org/maven2/org/scala-lang/jline/2.10.7/jline-2.10.7.pom";
+      sha256 = "16mg4b8c1m6gcq901wy6f6jpy8spw2yh909gi826xykq89ja94dg";
+    };
+    "nix-public/org/scala-lang/jline/2.10.7/jline-2.10.7.jar" = {
+      url = "https://repo1.maven.org/maven2/org/scala-lang/jline/2.10.7/jline-2.10.7.jar";
+      sha256 = "1cfk6w8ncx2g87grwdfmz6f76bn807saqik91iwcfv099b1jngw1";
+    };
+  };
+}
