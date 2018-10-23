e72351207892f9f4097effcbd50dd254511f39a6
David Waern <david.waern@gmail.com>
2018-10-23 19:24:33 +0200

    Update hashes

--- manual-repo.nix
+++ manual-repo.nix
@@ -15,11 +15,11 @@
     };
     "nix-public/org/fusesource/jansi/jansi/1.4/jansi-1.4.pom" = {
       url = "https://repo1.maven.org/maven2/org/fusesource/jansi/jansi/1.4/jansi-1.4.pom";
-      sha256 = "1cfk6whncx2g87grwdfmz6f76bn807saqik91iwcfv09911jngw1";
+      sha256 = "0rgprkbg4ljarf0x79snk2h1b0974glhl2fw1bxkxbw8k3ifda1s";
     };
     "nix-public/org/fusesource/jansi/jansi/1.4/jansi-1.4.jar" = {
       url = "https://repo1.maven.org/maven2/org/fusesource/jansi/jansi/1.4/jansi-1.4.jar";
-      sha256 = "13fk6whncx2g87grwdfmz6f76bn807saqik91iwcfv09911jngw1";
+      sha256 = "183ms545msn02fl0181rwbcifc8qy82rz4l6dglnhv9la8a1bnc2";
     };
   };
 }
