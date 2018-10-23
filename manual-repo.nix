106efd9d7e4472bedaf7a44ab44172fb17e3559a
David Waern <david.waern@gmail.com>
2018-10-23 19:20:08 +0200

    Update hash

--- manual-repo.nix
+++ manual-repo.nix
@@ -7,7 +7,7 @@
     # dependencies so sbt can build the sbt-compiler-interface (http://www.scala-sbt.org/0.13/docs/Compiler-Interface.html)
     "nix-public/org/scala-lang/jline/2.10.7/jline-2.10.7.pom" = {
       url = "https://repo1.maven.org/maven2/org/scala-lang/jline/2.10.7/jline-2.10.7.pom";
-      sha256 = "16mg4b8c1m6gcq901wy6f6jpy8spw2yh909gi826xykq89ja94dg";
+      sha256 = "0gr99hl9fbvmxzq9xsracqk5wgnqyc96pfpd08zrxlxqvp27n25m";
     };
     "nix-public/org/scala-lang/jline/2.10.7/jline-2.10.7.jar" = {
       url = "https://repo1.maven.org/maven2/org/scala-lang/jline/2.10.7/jline-2.10.7.jar";
