0f9384ee38ba1e9fb916f046a2660e18ff99a8d1
David Waern <david.waern@gmail.com>
2018-12-04 09:54:35 +0100

    Fixes to generate-repo.sh

--- generate-repos.sh
+++ generate-repos.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-set -x
-
 root=$(mktemp -d -t generate-repos-XXXXX)
 
 cleanup() {
   rm -fr "$root"
 }
 
-trap cleanup SIGINT SIGTERM EXIT
-
-#root="$(readlink -m "$(pwd)")"
-echo $root
+#trap cleanup SIGINT SIGTERM EXIT
 
 export sbt_global_base="$root/nix-sbt-global-base"
 mkdir -p "$sbt_global_base"
@@ -20,13 +15,15 @@ mkdir -p "$sbt_preloaded"
 
 export HOME="$root"
 
-mkdir -p $root/.sbt
+mkdir -p "$root/.sbt"
 ln -sT "$sbt_preloaded" "$root/.sbt/preloaded"
-
-mkdir -p $root/.coursier/cache/v1/
-
 
 export JAVA_OPTS="-Dsbt.preloaded=$sbt_preloaded -Dsbt.global.base=$sbt_global_base -Dsbt.ivy.home=$root/.ivy2/ -Divy.home=$root/.ivy2/"
 export COURSIER_CACHE="$root/.coursier"
 
-nix-shell generate-repos.nix --run sbtix-gen-all2
+ls -la $root/.sbtix/plugins/global-plugin
+ls -la $root/.sbtix/plugins
+nix-shell generate-repos.nix --run sbtix-gen-all2 || true
+tree "$root"
+ls -la "$root"
+tree "$root/.coursier"
